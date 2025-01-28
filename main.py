70e636d84d6f5b2d0dae53910cd7fc355ef9dae9
plbranson <plbranson@patrickbranson.com>
2025-01-28 03:46:47 -0500

    Setup clock

--- main.py
+++ main.py
@@ -12,5 +12,57 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
+import tkinter as tk
+from time import strftime
+from tkinter.font import Font
+
+
+class DigitalClock:
+    """
+    Digital Clock Graphical User Interface
+    """
+
+    __label: tk.Label
+    """
+    The clock label
+    """
+
+    __master: tk.Tk
+    """
+    The main window
+    """
+
+    def __init__(self, master: tk.Tk):
+        """
+        Initializes the Digital Clock
+
+        :param master: the main window
+        """
+        self.__master = master
+        self.__master.title(string="Digital Clock")
+        self.__master.configure(background="green")
+        self.__master.geometry(newGeometry="300x100")
+        self.__master.resizable(width=False, height=False)
+
+        self.__label = tk.Label(master=self.__master, background="black", foreground="cyan",
+                                font=Font(family="ariel", size=30, weight="bold"), relief="flat")
+        self.__label.place(x=20, y=20)
+        self.update()
+
+    # noinspection PyTypeChecker
+    def update(self) -> None:
+        """
+        Updates the time
+
+        :return: None - "void" function
+        """
+        current_time: str = strftime("%H: %M: %S\n %m-%d-%Y")
+        self.__label.configure(text=current_time)
+        self.__label.after(ms=80, func=self.update)
+        self.__label.pack(anchor="center")
+
+
 if __name__ == "__main__":
-    print("Hello, World!")
+    root: tk.Tk = tk.Tk()
+    DigitalClock(root)
+    root.mainloop()
